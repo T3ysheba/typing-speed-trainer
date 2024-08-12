c8176d531d0f39303c6f1ed83de7bb82c9fae57b
T3ysheba <mkoyansergey623@gmail.com>
2024-08-12 19:39:05 +0400

    added responsive

--- src/components/atom/FilterBar/index.tsx
+++ src/components/atom/FilterBar/index.tsx
@@ -6,40 +6,44 @@ import styles from './FilterBar.module.scss'
 
 const Filters: FC<TFiltersProps> = ({ textType, textToggler, onTimeSetClick, totalTime }) => (
   <div className={styles.wrapper}>
-    <p
-      role='button'
-      className={classNames(styles.wrapper__text, {
-        [styles.wrapper__text__active]: textType.includes('punctuations'),
-      })}
-      onClick={() => textToggler('punctuations')}
-    >
-      @ Punctuations
-    </p>
-    <p
-      role='button'
-      className={classNames(styles.wrapper__text, {
-        [styles.wrapper__text__active]: textType.includes('numbers'),
-      })}
-      onClick={() => textToggler('numbers')}
-    >
-      # Numbers
-    </p>
-
-    <p className={styles.wrapper__text}>|</p>
-
-    <p className={styles.wrapper__text}>Time:</p>
-    {[120, 60, 30, 10].map(time => (
+    <div className={styles.wrapper__row}>
       <p
-        key={time}
         role='button'
-        onClick={() => onTimeSetClick(time)}
         className={classNames(styles.wrapper__text, {
-          [styles.wrapper__text__active]: totalTime === time,
+          [styles.wrapper__text__active]: textType.includes('punctuations'),
         })}
+        onClick={() => textToggler('punctuations')}
       >
-        {time}
+        @ Punctuations
       </p>
-    ))}
+      <p
+        role='button'
+        className={classNames(styles.wrapper__text, {
+          [styles.wrapper__text__active]: textType.includes('numbers'),
+        })}
+        onClick={() => textToggler('numbers')}
+      >
+        # Numbers
+      </p>
+    </div>
+
+    <div className={styles.wrapper__line} />
+
+    <div className={styles.wrapper__row}>
+      <p className={styles.wrapper__text}>Time:</p>
+      {[120, 60, 30, 10].map(time => (
+        <p
+          key={time}
+          role='button'
+          onClick={() => onTimeSetClick(time)}
+          className={classNames(styles.wrapper__text, {
+            [styles.wrapper__text__active]: totalTime === time,
+          })}
+        >
+          {time}
+        </p>
+      ))}
+    </div>
   </div>
 )
 
